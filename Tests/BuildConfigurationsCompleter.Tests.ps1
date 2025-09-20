01d6efe5b7381110cd75cca93f5962b80f1f6e15
Mark Schofield <34426337+MarkSchofield@users.noreply.github.com>
2025-09-20 12:10:42 -0700

    Canonicalize -Preset,-Configuration; wildcard support

--- Tests/BuildConfigurationsCompleter.Tests.ps1
+++ Tests/BuildConfigurationsCompleter.Tests.ps1
@@ -11,7 +11,7 @@ BeforeAll {
 
 Describe 'BuildConfigurationsCompleter' {
     It 'Returns the default configurations when no preset is specified' {
-        $Completions = Get-CommandCompletion "Build-CMakeBuild -Configurations "
+        $Completions = Get-CommandCompletion "Build-CMakeBuild -Configuration "
         $Completions.CompletionMatches.Count | Should -Be 4
         $Completions.CompletionMatches[0].CompletionText | Should -Be 'Release'
         $Completions.CompletionMatches[1].CompletionText | Should -Be 'Debug'
@@ -20,7 +20,7 @@ Describe 'BuildConfigurationsCompleter' {
     }
 
     It 'Returns the default configurations when no preset is specified, filtered by the word to complete' {
-        $Completions = Get-CommandCompletion "Build-CMakeBuild -Configurations D"
+        $Completions = Get-CommandCompletion "Build-CMakeBuild -Configuration D"
         $Completions.CompletionMatches.Count | Should -Be 1
         $Completions.CompletionMatches[0].CompletionText | Should -Be 'Debug'
     }
